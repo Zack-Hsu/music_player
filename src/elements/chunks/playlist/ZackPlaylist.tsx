fdf5592bb57092c1816746925b45c560fddea736
Zack-Hsu <zackxu841127@gmail.com>
2023-09-08 17:30:06 +0800

    first-commit

---
+++ src/elements/chunks/playlist/ZackPlaylist.tsx
@@ -0,0 +1,14 @@
+export default [
+    {
+        name: "Yuuri",
+        audioSrc: "/assets/audio/compilation/yuuri_test/",
+        musicFile: "index.mp3",
+        musicPic: "index.png",
+    },
+    {
+        name: "Yoasobi 群青",
+        audioSrc: "/assets/audio/compilation/yoasobi-群青/",
+        musicFile: "index.mp3",
+        musicPic: "index.png",
+    },
+];
